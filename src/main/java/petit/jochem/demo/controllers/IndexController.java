f0efe3dc70477d7a058073a09efc31b14ceeae5c
mobapp09 <jochem_petit@msn.com>
2019-12-11 11:58:02 +0100

    vertaling toegevoegd, delete knop tegevoegd

--- src/main/java/petit/jochem/demo/controllers/IndexController.java
+++ src/main/java/petit/jochem/demo/controllers/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import petit.jochem.demo.model.Snack;
@@ -15,15 +16,12 @@ import java.io.PipedOutputStream;
 
 @Controller
 public class IndexController {
-
     @Autowired
     SnackDAO dao;
-
     @ModelAttribute(value = "allSnacks")
     public Iterable<Snack> getAllSnacks() {
         return dao.findAll();
     }
-
     @ModelAttribute(value = "newSnack")
     public Snack snackToSave() {
         return new Snack();
@@ -32,9 +30,7 @@ public class IndexController {
     @RequestMapping(value = {"", "/", "/index"}, method = RequestMethod.GET)
     public String showIndex(ModelMap map) {
         return "index";
-
     }
-
     @RequestMapping(value = {"", "/", "/index"}, method = RequestMethod.POST)
     public String saveSnack(@ModelAttribute("newSnack") @Valid Snack newSnack, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
@@ -42,6 +38,9 @@ public class IndexController {
         dao.save(newSnack);
         return "redirect:/index";
     }
-
-
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteSnack(@PathVariable(value = "id")int id){
+        dao.deleteById(id);
+        return "redirect:/index";
+    }
 }
